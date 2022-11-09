881389dfecc7c57524a0acaa8fce1372e8262b4d
maxvanwijnen <maxvanwijnen@gmail.com>
2022-11-10 00:32:26 +0100

    responsive gemaakt

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {BasketContext} from "../../context/BasketContext";
 import css from './basket.module.css';
 import BasketLine from "./BasketLine/BasketLine";
@@ -7,17 +7,28 @@ import PriceSummary from "./PriceSummary/PriceSummary.module";
 export const Basket = () => {
     const { basket } =useContext(BasketContext);
 
+    const [showBasketLines,toggleShowBasketLines] = useState(false);
+
+
+
+
     return(
-        <div className={css['basket-wrapper']}>
+        <div className={`${css['basket-wrapper']} ${showBasketLines && css['extend-it']}`}>
 
             {
                 basket.photoList.length > 0 &&
                 <>
-                <h2>Samenvatting</h2>
-                    <h3>Foto selectie</h3>
-                {basket.photoList.map(
-                    (photo) => <BasketLine key={photo.src} photo={photo} />
-                )}
+                <h2 className={css['basket-title']}>Samenvatting</h2>
+                    {!showBasketLines && <p className={css['extend-button']} onClick={()=>toggleShowBasketLines(!showBasketLines)}>[+] gekozen foto's tonen</p>}
+                    {showBasketLines && <p className={css['extend-button']} onClick={()=>toggleShowBasketLines(!showBasketLines)}>[-] gekozen foto's verbergen</p>}
+                    <section className={`${css['basket-line-wrapper']} ${showBasketLines && css['extended'] }`}>
+
+                        <h3>Foto selectie</h3>
+                        {basket.photoList.map(
+                            (photo) => <BasketLine key={photo.src} photo={photo} />
+                        )}
+                    </section>
+
                     <PriceSummary />
                     {basket.basketWarning  && <div className={css['basket-warning']}>{basket.basketWarning}</div>}
                     <button className={css['next-button']}>Volgende</button>
